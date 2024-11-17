485923c3094270627ad9c0e2ad46b553a5ce01a6
geokos009 <83821784+geokos009@users.noreply.github.com>
2024-11-17 17:40:10 +0530

    Update copied chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -1,51 +1,52 @@
 import streamlit as st
 from streamlit_chat import message
-#from langchain.chat_models import ChatOpenAi
+# from langchain.chat_models import ChatOpenAI
 from langchain_openai import ChatOpenAI
-#from langchain_google_genai import ChatGoogleGenerativeAI
+# from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.chains import ConversationChain
 from langchain.chains.conversation.memory import ConversationBufferWindowMemory
 
 import os
 
-# os.environ["OPEN_API_KEY"] = st.secrets["OPEN_API_KEY"]
+# os.environ["OPENAI_API_KEY"] = st.secrets["OPENAI_API_KEY"]
 
-#Initialize session state variables
+# Initialize session state variables
 if 'buffer_memory' not in st.session_state:
     st.session_state.buffer_memory = ConversationBufferWindowMemory(k=3, return_messages=True)
 
-if "messages" not in st.session_state.keys(): #Initialize the chat message history
+if "messages" not in st.session_state.keys(): # Initialize the chat message history
     st.session_state.messages = [
-        {"role": "assistannt", "content": "How can I help you today"}
+        {"role": "assistant", "content": "How can I help you today?"}
     ]
 
-# Initialialize ChatOpenAI and ConversationChain
-# llm = ChatOpenAI(modelname = "gpt-4o-mini")
+# Initialize ChatOpenAI and ConversationChain
+# llm = ChatOpenAI(model_name="gpt-4o-mini")
 # llm = ChatGoogleGenerativeAI(model = "gemini-pro")
 llm = ChatOpenAI(model = "meta-llama/Llama-3.2-90B-Vision-Instruct-Turbo",
-                 openai_api_key = st.secrets["TOGETHER_API_KEY"],
-                 openai_api_base = "https://api.together.xyz/v1"       
+                      openai_api_key = st.secrets["TOGETHER_API_KEY"] , ## use your key
+                      openai_api_base = "https://api.together.xyz/v1"
+
 )
 
-conversation = ConversationChain(memory = st.session_state.buffer_memory, llm = llm)
+conversation = ConversationChain(memory=st.session_state.buffer_memory, llm=llm)
 
-#Create User Interface
+# Create user interface
+st.title("🗣️ Conversational Chatbot")
+st.subheader("㈻ Simple Chat Interface for LLMs by Build Fast with AI")
 
-st.title("Conversation Chatbot")
-st.subheader("Simple Chat Interface for LLMs by GK")
 
-if prompt := st.chat_input("Your Question"): # Prompt for user input and save to chat history
+if prompt := st.chat_input("Your question"): # Prompt for user input and save to chat history
     st.session_state.messages.append({"role": "user", "content": prompt})
 
-for messages in st.session_state.messages: # Display prior chat messages
+for message in st.session_state.messages: # Display the prior chat messages
     with st.chat_message(message["role"]):
         st.write(message["content"])
 
 # If last message is not from assistant, generate a new response
-if st.session_state.messages[-1]["role"] != "assitant":
+if st.session_state.messages[-1]["role"] != "assistant":
     with st.chat_message("assistant"):
-        with st.spinner("Thinking"):
-            response = conversation.predict(imput = prompt)
+        with st.spinner("Thinking..."):
+            response = conversation.predict(input = prompt)
             st.write(response)
-            message = {"role": "assitant", "content": response}
+            message = {"role": "assistant", "content": response}
             st.session_state.messages.append(message) # Add response to message history
